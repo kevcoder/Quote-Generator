348dd02ea181a58e82fa4a5e60d0a12c9b791bfd
kevcoder <kevcodes@gmail.com>
2016-11-27 01:07:50 -0500

    javascript code updated

--- js/app.js
+++ js/app.js
@@ -1,4 +1,38 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+//document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+
+var markTwain = {
+		quote: "Don't go around saying the world owes you a living. The world owes you nothing. It was here first.",
+		source: 'Mark Twain',
+		citation: 'Blah'
+}
+
+var robertKennedy = {
+	quote: "Only those who dare to fail greatly can ever achieve greatly",
+	source: 'Robert F. Kennedy',
+	citation: 'blah'
+}
+
+var proverbs1728 = {
+	quote: "Even fools seem smart when they are quite",
+	source: ' Proverbs 17:28',
+	citation: 'blah'
+}
+
+var unknown = {
+	quote: "The best thing about the future is it comes one day at a time",
+	source: 'unknown',
+	citation: 'blah'
+}
+
+var JaneGoodall = {
+	quote: " If you really want something,& really work hard, & take advantage of opportunities, & never give up, you will find a way."
+	source: 'Jane Goodall',
+	citation: 'blah'
+
+}
+//Array of quotes, which will be used to be displayed on the page.
+var quotes = [markTwain, robertKennedy, proverbs1728,  , ""] 
