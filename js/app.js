d55c78d72570802431962e4b589530d09c5b9934
kevcoder <kevcodes@gmail.com>
2016-11-29 02:26:16 -0500

    complete function that allows you to print html <p> with object values concatenated

--- js/app.js
+++ js/app.js
@@ -11,25 +11,25 @@ var markTwain = {
 }
 
 var robertKennedy = {
-	quote: "Only those who dare to fail greatly can ever achieve greatly",
+	quote: "Only those who dare to fail greatly can ever achieve greatly.",
 	source: 'Robert F. Kennedy',
 	citation: 'blah'
 }
 
 var proverbs1728 = {
-	quote: "Even fools seem smart when they are quite",
+	quote: "Even fools seem smart when they are quite.",
 	source: ' Proverbs 17:28',
 	citation: 'blah'
 }
 
 var unknown = {
-	quote: "The best thing about the future is it comes one day at a time",
+	quote: "The best thing about the future is it comes one day at a time.",
 	source: 'unknown',
 	citation: 'blah'
 }
 
 var janeGoodall = {
-	quote: " If you really want something,& really work hard, & take advantage of opportunities, & never give up, you will find a way.",
+	quote: " If you really want something, and really work hard, & take advantage of opportunities, & never give up, you will find a way.",
 	source: 'Jane Goodall',
 	citation: 'blah'
 
@@ -39,22 +39,30 @@ var janeGoodall = {
 var quotes = [markTwain, robertKennedy, proverbs1728, unknown, janeGoodall];
 
 
-//Create a function called getRandomQuote that selects random quote from the quotes array and returns the random quote selected
+//Create a function called getRandomQuote that selects random quote object from the quotes array and returns the random quote object selected
 var getRandomQuote = function() {
-	var randomQuote = Math.floor(Math.random() * quotes.length) + 1;
-	return quotes[randomQuote];
+	var randomQuote = Math.floor(Math.random() * quotes.length);
+	var quoteSelected = quotes[randomQuote];
+	
+	return quoteSelected;
 }
 
 //Create a function called PrintQuote that calls getRandomQuote and stores it in a variable
 var printQuote = function(){
-
+	
+//not working have to already return the object with its properties separated at this point to store in this variable
+	var selectedQuote = getRandomQuote();
+	console.log("<p> " + printQuote().quote + " </p>");
+	console.log( printQuote().source);
+	console.log( printQuote().citation);
+	return selectedQuote;
 }
 
 
 
 
 
-console.log(getRandomQuote());
+
 
 
 
