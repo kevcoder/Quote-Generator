fac93d9fc623cbd568c12d15e694ea61ed2e21bd
kevcoder <kevcodes@gmail.com>
2016-11-30 21:20:44 -0500

    js functionality updated

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
-// event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
 
-//document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-//Create a list of objects which contains, 3 properties quote, source, citation which we will than place in an array with the rest of the objects and chose one randomly
+// Create a list of objects which contains, 3 properties quote, source, citation which we will than place in an array with the rest of the objects and chose one randomly
 var markTwain = {
 		quote: "Don't go around saying the world owes you a living. The world owes you nothing. It was here first.",
 		source: 'Mark Twain',
@@ -36,37 +33,39 @@ var janeGoodall = {
 }
  
  //Create Array named quotes which will hold all our objects
-var quotes = [markTwain, robertKennedy, proverbs1728, unknown, janeGoodall];
+var quotes = [
+	markTwain, 
+	robertKennedy, 
+	proverbs1728, 
+	unknown, 
+	janeGoodall
+
+	];
 
 
 //Create a function called getRandomQuote that selects random quote object from the quotes array and returns the random quote object selected
 var getRandomQuote = function() {
 	var randomQuote = Math.floor(Math.random() * quotes.length);
 	var quoteSelected = quotes[randomQuote];
-	
 	return quoteSelected;
 }
 
+
+
+
 //Create a function called PrintQuote that calls getRandomQuote and stores it in a variable
 var printQuote = function(){
-	
-//not working have to already return the object with its properties separated at this point to store in this variable
 	var selectedQuote = getRandomQuote();
-	console.log("<p> " + printQuote().quote + " </p>");
-	console.log( printQuote().source);
-	console.log( printQuote().citation);
-	return selectedQuote;
+	return document.write("<p>" + selectedQuote.quote + "</p>" +
+	 											"<p> " + selectedQuote.source + "</p>" + 
+	 											"<p>" + selectedQuote.citation+ "</p>" );  
 }
 
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-
-
-
-
-
-
-
-
+printQuote();
 
 
 
