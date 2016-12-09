3589d61cf4917a822d6cbc6a1fdc5433963e3f3e
kevcoder <kevcodes@gmail.com>
2016-12-09 00:47:25 -0500

    cleaned up the code and made it look neat

--- js/app.js
+++ js/app.js
@@ -1,33 +1,33 @@
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+
+document.getElementById('loadQuote').addEventListener("click", printQuote);
+
+
 // Create a list of objects which contains, 3 properties quote, source, citation which we will than place in an array with the rest of the objects and chose one randomly
 var markTwain = {
 		quote: "Don't go around saying the world owes you a living. The world owes you nothing. It was here first.",
-		source: 'Mark Twain',
-		citation: 'Blah'
+		source: "Mark Twain"
 }
 
 var robertKennedy = {
 	quote: "Only those who dare to fail greatly can ever achieve greatly.",
-	source: 'Robert F. Kennedy',
-	citation: 'blah'
+	source: "Robert F. Kennedy"
 }
 
 var proverbs1728 = {
 	quote: "Even fools seem smart when they are quite.",
-	source: ' Proverbs 17:28',
-	citation: 'blah'
+	source: "Proverbs 17:28"
 }
 
 var unknown = {
 	quote: "The best thing about the future is it comes one day at a time.",
-	source: 'unknown',
-	citation: 'blah'
+	source: "unknown"
 }
 
 var janeGoodall = {
-	quote: " If you really want something, and really work hard, & take advantage of opportunities, & never give up, you will find a way.",
-	source: 'Jane Goodall',
-	citation: 'blah'
-
+	quote: "If you really want something, and really work hard, & take advantage of opportunities, & never give up, you will find a way.",
+	source: "Jane Goodall"
 }
  
  //Create Array named quotes which will hold all our objects
@@ -37,9 +37,7 @@ var quotes = [
 	proverbs1728, 
 	unknown, 
 	janeGoodall
-
-	];
-
+];
 
 //Create a function called getRandomQuote that selects random quote object from the quotes array and returns the random quote object selected
 function getRandomQuote() {
@@ -52,51 +50,9 @@ function getRandomQuote() {
 function printQuote(){
 	var selectedQuote = getRandomQuote();
 	var message =  "";
-
 			message += '<p class = "quote">' + selectedQuote.quote + '</p>'
-			
 			message += '<p class = "source">' + selectedQuote.source + '</p>'
-
-			message += '<p class = "source">' + selectedQuote.citation + '</p>'
 			
 			document.getElementById('quote-box').innerHTML = message;
-
 }
-
-
 printQuote();
-
-// event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
-
-document.getElementById('loadQuote').addEventListener("click", printQuote);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
