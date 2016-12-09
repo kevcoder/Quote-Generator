a32203a224405984048966838dc955f5c0f27690
kevcoder <kevcodes@gmail.com>
2016-12-08 19:17:14 -0500

    js undefined bug sqashed

--- js/app.js
+++ js/app.js
@@ -1,8 +1,3 @@
-// event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
-
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
 // Create a list of objects which contains, 3 properties quote, source, citation which we will than place in an array with the rest of the objects and chose one randomly
 var markTwain = {
 		quote: "Don't go around saying the world owes you a living. The world owes you nothing. It was here first.",
@@ -53,21 +48,28 @@ function getRandomQuote() {
 	return quoteSelected;
 }
 
-
-
-
 //Create a function called PrintQuote that calls getRandomQuote and stores it in a variable
 function printQuote(){
 	var selectedQuote = getRandomQuote();
-	var message;
+	var message =  "";
 
-			message += '<p class = "quote">' + selectedQuote.quote + '</p>';
-			message += '<p class= "source">' + selectedQuote.source + '</p>'
+			message += '<p class = "quote">' + selectedQuote.quote + '</p>'
+			
+			message += '<p class = "source">' + selectedQuote.source + '</p>'
+
+			message += '<p class = "source">' + selectedQuote.citation + '</p>'
+			
 			document.getElementById('quote-box').innerHTML = message;
+
 }
 
 
 printQuote();
+
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+
+document.getElementById('loadQuote').addEventListener("click", printQuote);
 
 
 
